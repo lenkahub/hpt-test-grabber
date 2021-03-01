edfd1c58d8c66a6fa4b4cc30d3b61f5046aaead0
lenkahub <lenkahub@centrum.cz>
2021-03-01 17:07:40 +0100

    Update run.php
    přidání názvu a hodnocení do výstupu

--- run.php
+++ run.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 // code here
 
-$res = 'https://www.czc.cz/acer-predator-triton-500-pt515-52-70hb-cerna/289883/produkt';
+        
+$res = 'https://www.czc.cz/microsoft-surface-go-2-4gb-64gb/287541/produkt';
 
 $ch = curl_init();
 
@@ -18,29 +19,33 @@ $dom = new DOMDocument();
 
 $xpath = new DOMXPath($dom);
 
-$nodeList = $xpath->query("//span[@class='price-vatin']");
-$node = $nodeList->item(1);
+$prices = $xpath->query("//span[@class='price-vatin']");
+$price = $prices->item(1);
 
-$nodeList2 = $xpath->query("//span[@class='pd-next-in-category__item-value']");
-$node2 = $nodeList2->item(0);
+$codes = $xpath->query("//span[@class='pd-next-in-category__item-value']");
+$code = $codes->item(0);
 
+$names = $xpath->query("//h1");
+$name = $names->item(0);
+        
+$ratings = $xpath->query("//span[@class='rating__label']");
+$rating = $ratings->item(0);
 
 $save = fopen("vstup.txt", 'a');
 
-fwrite($save, $node->nodeValue . "\r\n");
-fwrite($save, $node2->nodeValue);
+fwrite($save, $price->nodeValue . "\r\n");
+fwrite($save, $code->nodeValue);
 
 fclose($save);
 
 
-$vystup[] = ["cena" => $node->nodeValue, "kod vyrobce" => $node2->nodeValue];
+$vystup[] = ["cena" => $price->nodeValue, "kod vyrobce" => $code->nodeValue, "nazev" => $name->nodeValue, "hodnoceni" => $rating->nodeValue];
 
 $soubor = json_encode($vystup);
 
 $saveJson = fopen("vystup.json", 'a');
 fwrite($saveJson, $soubor);
 fclose($saveJson);
-
 
 // $dispatcher = new \HPT\Dispatcher( ... );
 // $dispatcher->run();
